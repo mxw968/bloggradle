38657126e6f23d002de21691f79fddf75ca0d235
mxw968 <870346137@qq.com>
2018-12-20 21:48:35 +0800

    完善BlogController

--- src/main/java/com/zj/bloggradle/controller/BlogController.java
+++ src/main/java/com/zj/bloggradle/controller/BlogController.java
@@ -1,13 +1,38 @@
 package com.zj.bloggradle.controller;
 
+import com.zj.bloggradle.domain.es.EsBlog;
+import com.zj.bloggradle.repository.es.EsBlogRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class BlogController {
+import java.util.List;
 
-    @RequestMapping("/hello")
-    public String hello() {
-        return "hello world";
+/**
+ * Blog 控制器
+ * @author 周杰
+ * @date 2018/12/20
+ */
+@RestController
+@RequestMapping("/blogs")
+public class BlogController {
+    @Autowired
+    private EsBlogRepository esBlogRepository;
+
+    @GetMapping
+    public List<EsBlog> list(@RequestParam(value = "title") String title,
+                             @RequestParam(value = "summary")String summary,
+                             @RequestParam(value = "content") String content,
+                             @RequestParam(value = "pageIndex", defaultValue = "0") int pageIndex,
+                             @RequestParam(value = "pageSize", defaultValue = "10") int pageSize) {
+        Pageable pageable = new PageRequest(pageIndex, pageSize);
+
+        Page<EsBlog> esBlogs = esBlogRepository.findDistinctEsBlogByTitleContainingOrSummaryContainingOrContentContaining(title, summary, content,pageable);
+        return esBlogs.getContent();
     }
 }
